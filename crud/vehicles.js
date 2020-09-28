4b3db3a248013c7fada2c90a54d64b9eb3e108d0
Dualem <pan.lukasz.wlosek@gmail.com>
2020-09-28 09:09:23 +0100

    update vehicle function added

--- crud/vehicles.js
+++ crud/vehicles.js
@@ -1,6 +1,7 @@
 const AWS = require('aws-sdk');
 const { v4: uuidv4 } = require('uuid');
 
+// connection with dynamodb
 const ddb = new AWS.DynamoDB.DocumentClient();
 const tableName = "Vehicles"
 
@@ -11,10 +12,16 @@ exports.handler = async (event) => {
         switch (event.httpMethod) {
            
             case 'GET':
-                data = await readData();
+                data = await readData(event);
                 return {statusCode:200, body:JSON.stringify(data)};
             case 'POST':
                 data = await createData(event);
+                return {statusCode:200, body:JSON.stringify(data)};
+            case 'PUT':
+                data = await updateData(event);
+                return {statusCode:200, body:JSON.stringify(data)};
+            case 'DELETE':
+                data = await deleteData(event);
                 return {statusCode:200, body:JSON.stringify(data)};
             default:
                 return{statusCode:404, body:`Unsupported method "${event.httpMethod}"`};
@@ -25,17 +32,26 @@ exports.handler = async (event) => {
     }
     
 };
-const readData = async() =>{
-    let params = {
-        TableName: tableName
-    }
-    return ddb.scan(params).promise();
-}
+// Get order with uuid provided in url
+const readData = async(event) =>{
+    let vehicleId = event.pathParameters.id;
 
+    let params = {
+        TableName: tableName,
+        Key:{
+            id:vehicleId
+        }
+    }
+    return ddb.get(params).promise().then(result=>{
+        return result.Item;
+    });
+}
+// Post data, returns uuid
 const createData = async (event) =>{
     
     let item = JSON.parse(event.body)
     item.id = uuidv4();
+    //add 3 day ttl to db
     let params = {
         TableName: tableName,
         Item: item
@@ -50,6 +66,42 @@ const createData = async (event) =>{
             
         }   
     }).promise().then(() =>{
-        return params.Item
+        return params.Item.id
     });
+}
+//Update data using uuid
+const updateData = async (event)=>{
+    let vehicleId = event.pathParameters.id;
+    let body = JSON.parse(event.body);
+    let manufacture = body.Manufacture;
+    let model = body.Model;
+    let price = body.Price;
+
+    let params = {
+        TableName:tableName,
+        Key:{
+            id:vehicleId
+        },
+        UpdateExpression: "set Manufacture = :r, Model=:p, Price=:a",
+        ExpressionAttributeValues:{
+            ":r":manufacture,
+            ":p":model,
+            ":a":price
+        },
+        ReturnValues:"UPDATED_NEW"
+    };
+
+    return ddb.update(params, function(err, data) {
+        if (err) {
+            console.error("Unable to update item. Error JSON:", JSON.stringify(err, null, 2));
+        } else {
+            console.log("Update item succeeded:", JSON.stringify(data, null, 2));
+        }
+    }).promise();
+
+}
+
+//Delete data function
+const deleteData = async (event) =>{
+    let vehicleId = event.pathParameters.id;
 }
